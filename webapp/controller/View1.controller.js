d34b0f25e1ab9fe29da81939a0a94fbe3117f73d
saikumardasari8888 <137864713+saikumardasari8888@users.noreply.github.com>
2026-07-08 22:08:55 +0530

    Update View1.controller.js

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,6 +1,8 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+
+], (Controller, JSONModel) => {
     "use strict";
 
     return Controller.extend("ns.html5module.controller.View1", {
